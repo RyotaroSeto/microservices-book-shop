99a65d398d2a254aa5e200df7f8cba0a6cb421f2
RyotaroSeto <hahahahawaiwai321@gmail.com>
2024-01-02 18:44:26 +0900

    Add book model, book repository, and book use cases

--- catalogue/go.mod
+++ catalogue/go.mod
@@ -3,6 +3,9 @@ module gihyo/catalogue
 go 1.21.4
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/pkg/errors v0.9.1
 	google.golang.org/grpc v1.60.1
 	google.golang.org/protobuf v1.31.0
 )
--- catalogue/main.go
+++ catalogue/main.go
@@ -1,95 +1,53 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
-	pb "gihyo/catalogue/proto/book"
+	"gihyo/catalogue/infrastructure/repository"
+	"gihyo/catalogue/interfaces"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
-	emptypb "google.golang.org/protobuf/types/known/emptypb"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
-
-type Book struct {
-	Id     int
-	Title  string
-	Author string
-	Price  int
-}
-
-var (
-	book1 = Book{
-		Id:     1,
-		Title:  "The Awakening",
-		Author: "Kate Chopin",
-		Price:  1000,
-	}
-	book2 = Book{
-		Id:     2,
-		Title:  "City of Glass",
-		Author: "Paul Auster",
-		Price:  2000,
-	}
-	books = []Book{book1, book2}
-)
-
-func getBook(i int) Book {
-	return books[i-1]
-}
-
-type server struct {
-	pb.UnimplementedCatalogueServer
-}
-
-func (s *server) GetBook(ctx context.Context, in *pb.GetBookRequest) (*pb.GetBookResponse, error) {
-	book := getBook(int(in.Id))
-
-	protoBook := &pb.Book{
-		Id:     int32(book.Id),
-		Title:  book.Title,
-		Author: book.Author,
-		Price:  int32(book.Price),
-	}
-
-	return &pb.GetBookResponse{Book: protoBook}, nil
-}
-
-func (s *server) ListBooks(ctx context.Context, in *emptypb.Empty) (*pb.ListBooksResponse, error) {
-	protoBooks := make([]*pb.Book, 0)
-
-	for _, book := range books {
-		protoBook := &pb.Book{
-			Id:     int32(book.Id),
-			Title:  book.Title,
-			Author: book.Author,
-			Price:  int32(book.Price),
-		}
-		protoBooks = append(protoBooks, protoBook)
-	}
-
-	return &pb.ListBooksResponse{Books: protoBooks}, nil
-}
 
 var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
 func main() {
+
 	flag.Parse()
+
+	dsn := os.Getenv("DATABASE")
+	if dsn == "" {
+		dsn = "catalogue_user:default_password@tcp(127.0.0.1:3306)/booksdb"
+	}
+
+	db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	sequenceRepository := repository.NewSequenceRepository(db)
+	bookRepository := repository.NewBookRepository(db, sequenceRepository)
+
+	server := interfaces.NewServer(interfaces.ServerParams{
+		BookRepository: bookRepository,
+	})
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterCatalogueServer(s, &server{})
-	reflection.Register(s)
+
 	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+
+	if err = server.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
