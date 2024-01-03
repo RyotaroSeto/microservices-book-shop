a575475b7b60de0dbfd8fb8b4b226dd9e7cabf92
RyotaroSeto <hahahahawaiwai321@gmail.com>
2024-01-03 15:07:11 +0900

    Delete Istio virtual services and ingress gateway
    This commit deletes the Istio virtual services and ingress gateway configurations for the common, frontend, and bff services.

--- infra/scripts/delete_all.sh
+++ infra/scripts/delete_all.sh
@@ -8,3 +8,7 @@ kubectl delete -f catalogue/k8s/
 kubectl delete -f order/k8s
 kubectl delete -f shipping/k8s/
 kubectl delete -f rabbitmq/k8s/
+
+kubectl delete -f common/istio/ingress-gateway.yaml
+kubectl delete -f frontend/istio/virtualservice.yaml
+kubectl delete -f bff/istio/virtualservice.yaml
